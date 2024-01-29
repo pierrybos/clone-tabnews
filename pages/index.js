daa1520afe81a0a416b893ae6c4e45db1a64856c
Bos, Pierry <pierry.bos@gmail.com>
2024-01-29 18:48:49 +0000

    subindo arquivos do server do  e do

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <h1>
+      Filipe, obrigado por ser essse ser humano excepcional
+      <br />e por essa oportunidade de aperfeiçoamento que estás
+      <br />
+      proporcionando para tanta gente.
+      <br />
+      Que Deus continue te abençoando muito,
+      <br />a ti e tua família.&#128513;
+    </h1>
+  );
+}
+
+export default Home;
